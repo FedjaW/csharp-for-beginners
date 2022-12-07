63356513a1f21d310b210792a82a447743eba5ba
Friedrich Wilms <f.wilms89@web.de>
2022-12-07 20:33:58 +0100

    comments

--- DelegatesLambdasGenerics/Generics.cs
+++ DelegatesLambdasGenerics/Generics.cs
@@ -13,11 +13,13 @@ public class LearnGenerics
         CalculateAndPrint(true, true, (a, b) => a && b);
     }
 
+    // CalculateAndPrint is now a generic method.
     static void CalculateAndPrint<T>(T x, T y, Combine<T> f)
     {
         var result = f(x, y);
         Console.WriteLine(result);
     }
 
+    // Combine is a generic delegate.
     delegate T Combine<T>(T a, T b); // T is type paramater
 }
