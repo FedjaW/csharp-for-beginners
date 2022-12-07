3953a771b948dff7fdc2fc9cbbb6a72fca95e828
Friedrich Wilms <f.wilms89@web.de>
2022-12-07 21:07:37 +0100

    wow a where clause

--- DelegatesLambdasGenerics/02-InAction/FilterAkaWhereClause/Program.cs
+++ DelegatesLambdasGenerics/02-InAction/FilterAkaWhereClause/Program.cs
@@ -6,24 +6,49 @@
     new(string.Empty, string.Empty, "Stromfront", true),
 };
 
-var result  = FilterHeroes(heroes, (hero) => hero.CanFly);
+var result  = Filter(heroes, (hero) => hero.CanFly);
 var heroesFiltered = string.Join(", ", result);
 Console.WriteLine(heroesFiltered);
 
-List<Hero> FilterHeroes(List<Hero> heroes, Filter f)
+// Old version (Step 1)
+// IEnumerable<Hero> FilterHeroesObsolete(IEnumerable<Hero> heroes, Filter f)
+// {
+//     var resultList = new List<Hero>();
+//     foreach (var hero in heroes)
+//     {
+//         if(f(hero))
+//         {
+//             resultList.Add(hero);
+//         }
+//     }
+
+//     return resultList;
+// }
+
+// Better version with yield syntax (Step 2)
+// IEnumerable<Hero> FilterHeroes(IEnumerable<Hero> heroes, Filter f)
+// {
+//     foreach (var hero in heroes)
+//     {
+//         if(f(hero))
+//         {
+//             yield return hero;
+//         }
+//     }
+// }
+
+// WOW! This is the "Where" clause from LINQ!
+IEnumerable<T> Filter<T>(IEnumerable<T> items, Filter<T> f)
 {
-    var resultList = new List<Hero>();
-    foreach (var hero in heroes)
+    foreach (var item in items)
     {
-        if(f(hero))
+        if(f(item))
         {
-            resultList.Add(hero);
+            yield return item;
         }
     }
-
-    return resultList;
 }
 
-delegate bool Filter(Hero h);
+delegate bool Filter<T>(T h);
 
 record Hero(string FirstName, string LastName, string HeroName, bool CanFly);
